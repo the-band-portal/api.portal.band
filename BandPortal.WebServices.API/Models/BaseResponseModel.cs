bf418576050a6571d2a06ccbdb07a32eadda04a7
Cerys <cerysamberlewis@gmail.com>
2026-02-22 19:18:26 +0000

    Create BaseResponseModel.cs

---
+++ BandPortal.WebServices.API/Models/BaseResponseModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BandPortal.WebServices.API.Models
+{
+    public abstract class BaseResponseModel
+    {
+        [Key]
+        [Required]
+        public required Guid Id { get; set; }
+
+        [Required]
+        public required Guid? BandId { get; set; }
+
+        [Required]
+        public required Guid? CreatedBy { get; set; }
+
+        [Required]
+        public required DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public required Guid? LastUpdatedBy { get; set; }
+
+        [Required]
+        public required DateTime? LastUpdatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public required Guid? DeletedBy { get; set; }
+
+        [Required]
+        public required DateTime? DeletedAt { get; set; }
+    }
+}
